49d55136fc9247668ec9dd34398c445f4576baad
danac4 <danac4@mail.tau.ac.il>
2023-01-10 19:09:53 +0200

    changed because of intenet

--- pcc_client.c
+++ pcc_client.c
@@ -14,14 +14,6 @@
 
 /* some of the code is from recreation 10 */
 
-//*
-// * sets N to file size and transfers file content to buffer.
-// * return buffer
-// */
-//char* process_file(FILE* fp, unsigned int* N){
-//    unsigned int buff_size;
-//
-//}
 
 /*
  * help from:
@@ -30,18 +22,19 @@
  */
 int main(int argc, char *argv[]){
     FILE *fp;
-    uint32_t N, C, buff_size;/*N is given file size in bytes, C is the number of printable characters received from the server */
-    u_long n;
-    char *buffer,*C_buff;
+    uint32_t N, C, buff_size, num;/*N is given file size in bytes, C is the number of printable characters received from the server */
+    char *buffer,*num_buff;
     int bytes_written = 0, bytes_read=0, curr_written = 0, curr_read = 0, sockfd = -1, left;
     struct sockaddr_in serv_addr;
     socklen_t addrsize = sizeof(struct sockaddr_in );
     if(argc != 4){
-        //incorrect number of arg
+        perror("Invalid number of arguments passed to pcc_client!");
+        exit(1);
     }
     fp = fopen(argv[3],"rb");
     if(!fp){
-        //error- cant open given file
+        perror("Failed to open given file");
+        exit(1);
     }
 
     /*get size of given file in bytes*/
@@ -53,14 +46,16 @@ int main(int argc, char *argv[]){
     buff_size = (N < BUFF_SIZE) ? N : BUFF_SIZE;
     buffer = malloc(buff_size);
     if(!buffer){
-        //error in allocating memory
+        perror("Failed to allocate memory for buffer");
+        exit(1);
     }
     memset(buffer, 0, buff_size);
 
     /*create socket*/
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        //cant create socket
+        perror("Failed to create socket");
+        exit(1);
     }
 
     /*set server params*/
@@ -69,23 +64,29 @@ int main(int argc, char *argv[]){
     serv_addr.sin_port = htons(atoi(argv[2]));
     if((inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)) != 1)
     {
-       //error
+        perror("Failed to convert ip address with inet_pton");
+        exit(1);
     }
 
     /* connect to server */
     if(connect(sockfd,(struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        //cant connect
+        perror("Failed to connect to server");
+        exit(1);
     }
 
     /* send N to server */
-    n = htonl(N);
-    while(bytes_written < N){
-        curr_written = write(sockfd, &n, N);
+    num = htonl(N);
+    num_buff = (char*)&num;
+    left = sizeof(num);
+    while(left > 0){
+        curr_written = write(sockfd, num_buff, left);
         if(curr_written <= 0){//<= or <
-            //error
+            perror("Write failed while trying to send N to server");
+            exit(1);
         }
-        bytes_written += curr_written;
+        num_buff += curr_written;
+        left -= curr_written;
     }
 
     /* send files contents to server */
@@ -93,12 +94,14 @@ int main(int argc, char *argv[]){
     while(bytes_read < N){
         curr_read = fread(buffer, 1,buff_size,fp);
         if(ferror(fp) != 0){ //?
-            //error in reading
+            perror("Failed to read file's content");
+            exit(1);
         }
         while(bytes_written < curr_read){
             curr_written = write(sockfd, buffer+bytes_written, curr_read - bytes_written);
             if(curr_written <= 0){ //<= or <
-                //error
+                perror("Failed to write file's content to server");
+                exit(1);
             }
             bytes_written += curr_written;
         }
@@ -110,25 +113,19 @@ int main(int argc, char *argv[]){
 
     /* get the number of printable characters received from the server */
     bytes_read = 0;
-    C_buff = (char *)&C;
+    num_buff = (char *)&C;
     left = sizeof(C);
     while(left > 0){
-        curr_read = read(sockfd, C_buff, left);
+        curr_read = read(sockfd, num_buff, left);
         if(curr_read <= 0){
-            //error
+            perror("Failed to read the number of printable characters from server");
+            exit(1);
         }
-        C_buff += curr_read;
+        num_buff += curr_read;
         left -= curr_read;
     }
-    C = htonl(C);
+    C = htonl(C); //?
     printf("# of printable characters: %u\n", C);
-    //open file for reading and to see that it can be open and detect errors in read
-    //create TCP connection to port argv[2] on the specified ip argv[1]
-    //transfer the contents of the file to the server over the tcp connection and receive count of printable characters from the server:
-    //send to the server the number of bytes that will be transferred (size of file)
-    //send file contents to server
-    //save server answer
-    //print to stdout "# of printable characters: %u\n" that got from server
     fclose(fp);
     close(sockfd);
     exit(0);
